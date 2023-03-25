23e212bd6386b10f30fef2447d3289275ae66525
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-03-25 10:21:24 +0200

    destream-chrome > dev: data

--- packages/destream-chrome/source/data/constants/index.ts
+++ packages/destream-chrome/source/data/constants/index.ts
@@ -1,14 +1,11 @@
 // #region module
-// export const DEFAULT_MESSAGER_ENDPOINT = 'messager.plurid.cloud';
-export const DEFAULT_MESSAGER_ENDPOINT = 'localhost:56865';
-
-export const DEFAULT_MESSAGER_TOKEN = '__TEST_MODE__';
+export const DEFAULT_MESSAGER_ENDPOINT = process.env.MESSAGER_ENDPOINT;
+export const DEFAULT_MESSAGER_TOKEN = process.env.MESSAGER_TOKEN;
 
 
-export const DEFAULT_API_ENDPOINT = 'https://api.plurid.com/graphql';
+export const DEFAULT_API_ENDPOINT = process.env.API_ENDPOINT;
 
 
-// export const DEFAULT_PUBLISH_ENDPOINT = 'https://api.plurid.com/graphql';
 export const DEFAULT_PUBLISH_ENDPOINT = 'http://localhost:3000/publish';
 
 
