40fdb1f0103a034ea3e8610ea60caaa7a1cc1fcd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-06-15 13:52:43 +0300

    destream-chrome > dev: contentscript/view/notifications

--- packages/destream-chrome/source/contentscript/view/notifications/index.ts
+++ packages/destream-chrome/source/contentscript/view/notifications/index.ts
@@ -8,19 +8,26 @@ export const getNotificationBox = () => {
         const newNotificationBox = document.createElement('div');
         newNotificationBox.id = NOTIFICATION_BOX_ID;
 
-        newNotificationBox.style.position = 'fixed';
-        newNotificationBox.style.bottom = '10px';
-        newNotificationBox.style.left = '10px';
-        newNotificationBox.style.zIndex = '999999';
-        newNotificationBox.style.width = '180px';
-        newNotificationBox.style.height = '100px';
-        newNotificationBox.style.overflow = 'auto';
-        newNotificationBox.style.display = 'flex';
-        newNotificationBox.style.flexFlow = 'column-reverse nowrap';
-        newNotificationBox.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        newNotificationBox.style.color = 'white';
-        newNotificationBox.style.fontSize = '10px';
-        newNotificationBox.style.fontFamily = `Ubuntu, -apple-system, BlinkMacSystemFont, sans-serif`;
+        newNotificationBox.style.cssText = `
+            position: fixed;
+            bottom: 10px;
+            left: 10px;
+            z-index: 999999;
+            width: 180px;
+            height: 100px;
+            overflow: auto;
+            display: flex;
+            flex-flow: column-reverse nowrap;
+            background-image: linear-gradient(
+                to top,
+                rgba(0, 0, 0, 1) 0%,
+                rgba(0, 0, 0, 0) 50%,
+                rgba(0, 0, 0, 0) 100%
+            );
+            color: white;
+            font-size: 10px;
+            font-family: Ubuntu, -apple-system, BlinkMacSystemFont, sans-serif;
+        `;
 
         document.body.appendChild(newNotificationBox);
 
@@ -31,20 +38,35 @@ export const getNotificationBox = () => {
 }
 
 
+export const resolveNotificationText = (
+    type: string,
+) => {
+    if (type.includes('Play')) return 'play';
+    if (type.includes('Pause')) return 'pause';
+    if (type.includes('Seek')) return 'seek';
+    if (type.includes('VolumeChange')) return 'volume change';
+    if (type.includes('Scroll')) return 'scroll';
+
+    return type;
+}
+
+
 class NotificationManager {
     private notifications: string[] = [];
 
 
     private render() {
-        const notificationBox = getNotificationBox();
-
-        const notification = document.createElement('div');
-
-        const notificationTimeBox = document.createElement('div');
         let mounted = true;
         let secondsPassed = 0;
-        notificationTimeBox.innerText = '0s ago';
+        const notificationIndex = this.notifications.length - 1;
 
+
+        const notificationBox = getNotificationBox();
+        const notification = document.createElement('div');
+
+
+        const notificationTimeBox = document.createElement('div');
+        notificationTimeBox.innerText = '0s ago';
         const timeInterval = setInterval(() => {
             if (!mounted) {
                 clearInterval(timeInterval);
@@ -55,24 +77,59 @@ class NotificationManager {
             notificationTimeBox.innerText = `${secondsPassed}s ago`;
         }, 1_000);
 
-        const notificationTextBox = document.createElement('div');
 
-        const notificationIndex = this.notifications.length - 1;
-        notificationTextBox.innerText = `${this.notifications[notificationIndex]}`;
+        const notificationTextBox = document.createElement('div');
+        notificationTextBox.innerText = resolveNotificationText(this.notifications[notificationIndex]);
+        notification.style.cssText = `
+            padding: 6px 12px;
+            display: flex;
+            gap: 12px;
+            align-items: center;
+            justify-content: space-between;
+        `;
+
+
+        const notificationReplayButton = document.createElement('div');
+        notificationReplayButton.innerHTML = `
+            <?xml version="1.0" encoding="iso-8859-1"?>
+            <svg
+                fill="#000000"
+                height="16px"
+                width="16px"
+                style="filter: invert(1);"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 300.003 300.003" xml:space="preserve"
+            >
+                <g>
+                    <g>
+                        <path d="M150.005,0C67.164,0,0.001,67.159,0.001,150c0,82.838,67.162,150.003,150.003,150.003S300.002,232.838,300.002,150
+                            C300.002,67.159,232.844,0,150.005,0z M230.091,172.444c-9.921,37.083-43.801,64.477-83.969,64.477
+                            c-47.93,0-86.923-38.99-86.923-86.923s38.99-86.92,86.923-86.92c21.906,0,41.931,8.157,57.228,21.579l-13.637,23.623
+                            c-11-11.487-26.468-18.664-43.594-18.664c-33.294,0-60.38,27.088-60.38,60.38c0,33.294,27.085,60.38,60.38,60.38
+                            c25.363,0,47.113-15.728,56.038-37.937h-20.765l36.168-62.636l36.166,62.641H230.091z"/>
+                    </g>
+                </g>
+            </svg>
+        `;
+
+        const handleClick = () => {
+            console.log('clicked');
+        }
+        notificationReplayButton.addEventListener('click', handleClick);
+
 
         notification.appendChild(notificationTimeBox);
         notification.appendChild(notificationTextBox);
-
-        notification.style.padding = '6px 12px';
-        notification.style.display = 'flex';
-        notification.style.gap = '12px';
-
-        // Add a replay button
+        notification.appendChild(notificationReplayButton);
 
         notificationBox.appendChild(notification);
 
+
         setTimeout(() => {
             mounted = false;
+
+            notificationReplayButton.removeEventListener('click', handleClick);
             notification.remove();
             this.notifications[notificationIndex] = undefined;
             this.checkNotificationsToBeRendered();
